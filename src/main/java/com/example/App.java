a59b56040c97491f8405425cbe6c699102368882
Adidas15 <adidasglhf@gmail.com>
2024-03-11 13:45:30 -0700

    Initial selenium setup

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,18 +1,24 @@
 package com.example;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("chromedriver","chromedriver-win64");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-
+        // driver.get("https://www.amazon.com/Air-Jungles-Freshener-Refresher-Eliminator/product-reviews/B07LGL6LSF/ref=cm_cr_arp_d_paging_btm_next_1?ie=UTF8&reviewerType=all_reviews&pageNumber=1");
+        // 
+        for (int i=1; i < 5; i++) {
+            driver.get("https://www.amazon.com/Air-Jungles-Freshener-Refresher-Eliminator/product-reviews/B07LGL6LSF/ref=cm_cr_arp_d_paging_btm_next_"+i+"?ie=UTF8&reviewerType=all_reviews&pageNumber="+i);
+            Thread.sleep(5000);
+        }
         driver.quit();
         
-        System.out.print("Finished");
+        // System.out.print("Finished");
     }
 }
